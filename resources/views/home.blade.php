42ff83d03c2dab48df084766cc90a42627e0033b
Remial Jude <ramenizing@gmail.com>
2022-07-25 22:20:04 +0800

    basta amo na ito boi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,55 +13,79 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<style>
-    #welcome-section p {
-        margin: 0;
-        padding: 0;
-    }
-</style>
+
 
 </head>
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #070372; padding-right: 100px;">
-            <a class="navbar-brand" href="#">LOGO</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto font-weight-semibold">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Advising</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
-                    </li>
-                </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #070372;">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="https://www.lnu.edu.ph/images/logo.png" alt="" style="height:    30px; width: 5vh;"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav font-weight-semibold">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Advising</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FAQ's</a>
+                            <div class="mx-5">
+                                <div class="mx-5">
+                                    <div class="mx-5">
+                                        <div class="mx-5">
+                                            <div class="mx-5">
+                                                <div class="mx-5">
+                                                    <div class="mx-5">
+                                                        <div class="mx-5">
+                                                            <div class="mx-5">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
             </div>
         </nav>
     </header>
 
-    <main class="mt-5">
-        <div class="container">
-            <section id="welcome-section" class="text-center">
-                <h3>Welcome to LNU Pre-registration and Advising System</h3>
-                <p>The pre-online enrollment for 1st Semester - S.Y. 2022-2023 is currently on-going!</p>
-                <p>Pre-Enrollment Period: July 18 - August 7, 2022</p>
-                <p>Enrollment Period: August 08-19, 2022</p>
-                <p>Classes Start: August 22, 2022</p>
-            </section>
-            <section id="announcement-section">
 
-            </section>
-        </div>
+
+    <main class="bg-dark">
+        <div class="bg-image img-fluid" style="background-image: url('');background-repeat: no-repeat; background-position: center;  background-size: fixed;
+  min-height: 90vh; ">
+
+            <div class="text-white" class="p-5" class="container">
+                <section id="welcome-section" class="p-5" class="text-left">
+                    <h3 class="text-primary">Welcome to <span class="text-warning">LNU-PAMS</span></h3>
+                    <p>The pre-online enrollment for 1st Semester - S.Y. 2022-2023 is</p>
+                    <p>currently on-going! Pre-Enrollment Period: July 18 - August 7, 2022</p>
+                    <p>Enrollment Period: August 08-19, 2022 Classes Start: August 22, 2022</p>
+                </section>
+                <div class="ml-5" class="content">
+                    <button type="button" class="btn btn-primary btn-lg mt-4" id="right-panel-link" href="#right-panel">LOG IN </button>
+                    <div>
+                        <div range-slider floor="0" ceiling="19" dragstop="true" ng-model-low="lowerValue" ng-model-high="upperValue"></div>
+                    </div>
+
+
+
+
 
     </main>
-
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
